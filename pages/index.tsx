c689052c4cb87ee05405079447484ef41881b699
ruobaole <daisyrpanglive@live.com>
2022-07-01 18:42:20 +0800

    :iphone: home page responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,31 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
-import { useMemo } from 'react'
 
 interface HomeFeatureSectionProps {
   title: string
   description: string
   link?: string
-  rightSided?: boolean
 }
 
 const HomeFeatureSection = ({
   title,
   description,
   link,
-  rightSided,
 }: HomeFeatureSectionProps): JSX.Element => {
-  const desArea: JSX.Element = useMemo(
-    () => (
-      <div className="w-1/3">
-        <h2 className="font-bold text-3xl text-gray-700 mb-4">{title}</h2>
-        <p className="text-gray-500 leading-relaxed text-lg font-['Inter var']">
+  return (
+    <section className="group flex flex-wrap flex-col lg:flex-row lg:justify-between lg:items-center py-12 md:py-16 lg:py-24 max-w-7xl">
+      <div className="mb-8 lg:mb-0 lg:w-1/3 lg:group-even:order-2">
+        <h2 className="font-bold text-xl md:text-3xl text-gray-700 mb-4">
+          {title}
+        </h2>
+        <p className="text-gray-500 leading-relaxed text-base md:text-lg font-['Inter var']">
           {description}
         </p>
         {link && (
-          <div className="mt-6">
+          <div className="mt-3 lg:mt-6">
             <a
-              className="text font-bold text-cyan-500"
+              className="text text-sm md:text-base font-bold text-cyan-500"
               href={link}
               target="_blank"
               rel="noreferrer"
@@ -36,23 +35,9 @@ const HomeFeatureSection = ({
           </div>
         )}
       </div>
-    ),
-    [title, description, link]
-  )
-
-  const imgArea: JSX.Element = useMemo(
-    () => (
-      <div className="w-3/5 border border-solid border-gray-400 h-[420px] rounded-md overflow-hidden order-none">
+      <div className="lg:w-3/5 border border-solid border-gray-400 h-[420px] rounded-md overflow-hidden order-none">
         screenshot or inline component for the freature
       </div>
-    ),
-    []
-  )
-
-  return (
-    <section className="flex flex-wrap justify-between items-center py-24 max-w-7xl">
-      {rightSided === true ? imgArea : desArea}
-      {rightSided === true ? desArea : imgArea}
     </section>
   )
 }
@@ -66,6 +51,7 @@ const Home: NextPage = () => {
           name="description"
           content="a next-generation AutoML SaaS from GDDi"
         />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -79,7 +65,7 @@ const Home: NextPage = () => {
               <b>GDDi</b>
             </a>
           </div>
-          <div className="flex items-center flex-grow-0 flex-shrink-0 content-end">
+          <div className="hidden md:flex items-center flex-grow-0 flex-shrink-0 content-end">
             <a
               className="font-medium inline-block px-4 text-gray-700"
               href="https://github.com/ruobaole/gddi-breeze"
@@ -97,27 +83,27 @@ const Home: NextPage = () => {
       </nav>
 
       <div className="bg-cyan-50 text-center px-20 py-8 select-none">
-        <h1 className="font-semibold font-logo text-[12rem] text-cyan-400">
+        <h1 className="font-semibold font-logo text-[6rem] md:text-[10rem] lg:text-[12rem] text-cyan-400">
           breeze
         </h1>
-        <h2 className="text-gray-700 mt-2">
+        <h2 className="text-gray-700 mt-2 text-sm md:text-base">
           An easy-to-use AutoML SaaS Platform
         </h2>
         <div className="mt-6">
           <Link href="/">
-            <a className="group rounded-md cursor-pointer inline-block font-medium mx-1 py-3 px-7 relative transition-none bg-cyan-600 hover:bg-cyan-500 text-gray-50">
+            <a className="group rounded-md cursor-pointer inline-block font-medium mx-1 py-2 md:py-4 px-4 md:px-7 relative transition-none bg-cyan-600 hover:bg-cyan-500 text-gray-50">
               <div className="flex items-center">
-                <span className="group-hover:motion-safe:animate-bounce-quick group-focus:motion-safe:animate-bounce-quick text-2xl mr-2">
+                <span className="group-hover:motion-safe:animate-bounce-quick group-focus:motion-safe:animate-bounce-quick text-xl md:text-2xl mr-2">
                   👉🏻
                 </span>
-                <span>Get Started</span>
+                <span className="text-sm md:text-base">Get Started</span>
               </div>
             </a>
           </Link>
         </div>
       </div>
 
-      <main className="flex font-home mx-16 my-0 px-0 py-4 flex-grow flex-col justify-center items-center">
+      <main className="flex font-home mx-2 md:mx-16 my-0 px-0 py-4 flex-grow flex-col justify-center items-center">
         <HomeFeatureSection
           title="feature section #1"
           description="Describe feature #1 in some concise languages Describe feature #1
@@ -135,7 +121,6 @@ const Home: NextPage = () => {
               feature #1 in some concise languages"
           link="https://nextjs.org/learn"
           key={'feature section #2'}
-          rightSided
         />
       </main>
 
